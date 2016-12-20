4c1210c0f8fca05159c8ddcbbad06b3846046d60
Nero Zuo <502350123@qq.com>
2016-12-20 20:09:48 +0800

    add create function

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,2 +1,8 @@
 class AudiosController < ApplicationController
+  def create
+    youtubuUrl = params[:q]
+    youtubuCommand = "youtube-dl -f 140 \"" + youtubuUrl + "\""
+    `"#{youtubuCommand}"`
+    redirect_to root_path
+  end
 end
