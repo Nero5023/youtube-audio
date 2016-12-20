128b27ed429ee1daf0d05b8e446147d96ea9e3a5
Nero Zuo <502350123@qq.com>
2016-12-20 20:13:08 +0800

    fix not install bug

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -2,7 +2,7 @@ class AudiosController < ApplicationController
   def create
     youtubuUrl = params[:q]
     youtubuCommand = "youtube-dl -f 140 \"" + youtubuUrl + "\""
-    `"#{youtubuCommand}"`
+    `#{youtubuCommand}`
     redirect_to root_path
   end
 end
